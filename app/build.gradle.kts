cb5e0c1a90c80a6ddb19300a4eb4819c68f77c24
Rafihul <rafihul2905@gmail.com>
2025-05-20 08:20:59 +0700

    Add Room persistence library dependencies
    This commit introduces the Room persistence library by adding the following dependencies:
    - `androidx.room:room-runtime:2.7.1` for the core Room functionality.
    - `androidx.room:room-compiler:2.7.1` (via ksp) for annotation processing.
    - `androidx.room:room-ktx:2.7.1` for Kotlin extensions.
    - `androidx.annotation:annotation:1.9.1` as a dependency for Room.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.56.2")
     ksp("com.google.dagger:hilt-android-compiler:2.56.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    implementation("androidx.room:room-runtime:2.7.1")
+    ksp("androidx.room:room-compiler:2.7.1")
+    implementation("androidx.room:room-ktx:2.7.1")
+    implementation("androidx.annotation:annotation:1.9.1")
 }
